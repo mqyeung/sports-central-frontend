aae5eadeed8f2bf3b56aa98e0b39ba4b02ea77ba
mqyeung <mqyeung@gmail.com>
2023-02-28 05:17:01 -0800

    fixed issue where homepage would not load

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,12 @@
 const home = () => {
-return(
-    <>
-    <div class="body">
-    <h1 class = "title">sports wee</h1>
-    <p class = "text"> you want to play lets play </p>
-    {/*<iframe class = "text" width="560" height="315" src="https://www.youtube.com/embed/e82VE8UtW8A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>*/}
-    </div>
-    </>
-
-)
-}
-export default home;
+    return(
+        <>
+        <div class="body">
+        <h1 class = "title">sports central</h1>
+        <p class = "text"> eeeeeeeee </p>
+        </div>
+        </>
+    
+    )
+    }
+    export default home;
--- src/pages/news.js
+++ src/pages/news.js
@@ -4,7 +4,6 @@ const news = () => {
         <div class="body">
         <h1 class = "title">news</h1>
         <p class = "text"> eeeeeeeee </p>
-        {/*<iframe class = "text" width="560" height="315" src="https://www.youtube.com/embed/e82VE8UtW8A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>*/}
         </div>
         </>
     
