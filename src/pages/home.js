5c6406e070a39a4979837f6cb05b3dc437eac559
mqyeung <mqyeung@gmail.com>
2023-02-28 20:21:38 -0800

    working responsive dropdown menu for narrow displays

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,10 +3,13 @@ const home = () => {
         <>
         <div class="body">
         <h1 class = "title">sports central</h1>
-        <p class = "text"> eeeeeeeee </p>
+        <p class = "text"> you want to play, let's play </p>
+            <div align="center">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/UgZ2-jb76K8"></iframe>
+            </div>
         </div>
+
         </>
-    
     )
     }
     export default home;
